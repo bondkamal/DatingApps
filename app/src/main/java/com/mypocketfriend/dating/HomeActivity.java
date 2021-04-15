6805a8f8bed5a8fe8d2859cb1d021768e0ac1918
Surajit221 <61044806+Surajit221@users.noreply.github.com>
2021-04-15 17:23:06 +0530

    Three

--- app/src/main/java/com/mypocketfriend/dating/HomeActivity.java
+++ app/src/main/java/com/mypocketfriend/dating/HomeActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
